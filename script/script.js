daba9c9243be988c31b6089c60fd2e2b32e4611a
gabrielalvino <gabrielalvino2008@gmail.com>
2022-07-15 12:17:22 -0300

    create random id

--- script/script.js
+++ script/script.js
@@ -1,1 +1,48 @@
 ﻿
+const  Front = "card_front";
+const Back = "card_back";
+
+let techs = [
+
+	"bootstrap",
+	"css",
+	"git",
+	"html",
+	"java",
+	"javascript",
+	"jquery",
+	"react",
+	"ruby",
+	"vue",
+]
+
+createCardsFromTechs(techs);
+
+function createCardsFromTechs(techs) {
+
+	let cards = [];
+
+	for (let tech in techs) {
+		cards.push(createPairFromTech(tech))
+	}
+	console.log(cards)
+}
+
+function createPairFromTech(tech) {
+
+	return {
+		id: createIdWithTech(tech),
+		icon: tech,
+		flipped: false,
+	},
+	{
+		id: createIdWithTech(tech),
+		icon: tech,
+		flipped: false,
+	}
+}
+
+function createIdWithTech(tech){
+
+	return tech + parseInt(Math.random() *1000);
+}
